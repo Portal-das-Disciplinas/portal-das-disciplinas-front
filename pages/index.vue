56f796cd3836f5e03346f25622f16429f17d365d
João Victor Dantas <dantjv@outlook.com>
2021-07-09 10:28:37 -0300

    Waves - second section + css styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <script src="https://kit.fontawesome.com/fae7e0756f.js" crossorigin="anonymous"></script>
     <Navbar/>
+    <!-- First section - Navbar + Video + Short description -->
     <section class="mt-4 mb-5" >
       <b-container>
         <b-row>
@@ -16,18 +16,29 @@
       </b-container>
     </section>
 
-    <section class="bg-alt section-gap">
+    <!-- Second section - description + Cards + waves -->
+
+    <section class="bg-alt p-relative">
+      <div class="wave-container wave-top">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="var(--azul-waves-alt)" fill-opacity="1" d="M0,224L48,213.3C96,203,192,181,288,186.7C384,192,480,224,576,234.7C672,245,768,235,864,218.7C960,203,1056,181,1152,192C1248,203,1344,245,1392,266.7L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+      </div>
 		<b-container>
 			<b-row class="mb-5">
-				<h1 class="w-100 display-title-gallery">Disciplinas Cadastradas</h1>
-				<p class="gallery-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati possimus eos expedita eum veritatis quasi quae architecto exercitationem molestiae tempore! Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+        <div class="index-100 mt-5">
+          <h1 class="w-100 display-title-gallery">Disciplinas Cadastradas</h1>
+          <p class="gallery-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati possimus eos expedita eum veritatis quasi quae architecto exercitationem molestiae tempore! Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+        </div>
 			</b-row>
+      <hr class="index-100 p-relative index-100 hr-cards w-100">
 			<b-row>
 				<div id="cards-div" class="d-flex align-items-center justify-content-between flex-wrap align-content-lg-center">
 					<li v-for="item in items" :key="item"><Card-alt class="my-3" Professor="Francisco Carlos" Name="TI e Sociedade" Synopsis="Lorem ipsum dolor sit, amet tetur adipisicing elit. Amet dis cap diem"/></li>
 				</div>
 			</b-row>
 		</b-container>
+    <div class="wave-container">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="var(--azul-waves)" fill-opacity="1" d="M0,192L48,208C96,224,192,256,288,224C384,192,480,96,576,96C672,96,768,192,864,202.7C960,213,1056,139,1152,96C1248,53,1344,43,1392,37.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </div>
     </section>
     <Footer/>
   </div>
@@ -45,6 +56,7 @@ export default {
 </script>
 
 <style>
+
   @import '../assets/main.css';
   *{
     margin: 0;
