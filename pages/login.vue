badb9029a4f083c7ad07ef0208b103dda00cc936
João Victor Dantas <dantjv@outlook.com>
2021-07-14 10:41:20 -0300

    Definicao BG login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        sonibleinesini
+        <Navbar/>
+        <div class="bg-login">
+        </div>
+        <Footer/>
     </div>
 </template>
 
@@ -12,4 +15,12 @@ export default {
 
 <style scoped>
 
+.bg-login{
+    width: 100%;
+    height: calc(100vh);
+    background: linear-gradient(60deg, rgba(1, 75, 140, 0.8), rgba(236, 54, 115, 0.651), rgba(124, 54, 236, 0.75)), url('../assets/img/bg-imd.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
 </style>
